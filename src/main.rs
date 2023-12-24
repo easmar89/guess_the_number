d7694a78a7ff43d22f3a2559b126dc8192348fa9
Elie Asmar <elieasmar89@gmail.com>
2023-12-24 14:10:50 +0100

    added colors and input validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
@@ -16,13 +17,19 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input! Try again: ");
+                continue;
+            }
+        };
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small..try again: "),
             Ordering::Greater => println!("Too big...try again: "),
             Ordering::Equal => {
-                println!("YES! Congrats");
+                println!("{}", "YES! Congrats".green().bold());
                 break;
             }
         }
